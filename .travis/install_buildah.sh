5a13369a6fd570c1b1165d2bd15fcdaef2565eea
Laszlo Janosi <laszlo.janosi1@gmai.com>
2019-01-18 22:25:19 +0000

    docker must be removed to test buildah

--- .travis/install_buildah.sh
+++ .travis/install_buildah.sh
@@ -2,20 +2,19 @@
 
 if [[ $WITH_BUILDAH == 'Y' ]]; then
     
-    sudo systemctl stop docker
-
+    sudo rm -f  "$(command -v docker)"
+	
     sudo apt-get -y install software-properties-common
     sudo add-apt-repository -y ppa:alexlarsson/flatpak
     sudo add-apt-repository -y ppa:gophers/archive
     sudo apt-add-repository -y ppa:projectatomic/ppa
     sudo apt-get -y -qq update
     sudo apt-get -y install bats btrfs-tools git libapparmor-dev libdevmapper-dev libglib2.0-dev libgpgme11-dev libostree-dev libseccomp-dev libselinux1-dev skopeo-containers go-md2man
-    sudo apt-get -y install golang-1.10
-
+    
     mkdir -p "$GOPATH/src/github.com/containers/buildah"
     git clone https://github.com/containers/buildah "$GOPATH/src/github.com/containers/buildah"
     cd "$GOPATH/src/github.com/containers/buildah" || exit
-    PATH=/usr/lib/go-1.10/bin:$PATH make runc all SECURITYTAGS="apparmor seccomp"
+    make runc all SECURITYTAGS="apparmor seccomp"
     sudo make install install.runc
     
     sudo cat > /etc/containers/registries.conf <<- "EOF"

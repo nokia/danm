3c8a60f76ced12559aea70115a1a4adcc32f7e4c
kopcso <tamas.kopcso@nokai.com>
2019-03-07 20:45:36 +0100

    correct PopDevice (panic case)
    update TestPopDevice

--- pkg/danm/danm_test.go
+++ pkg/danm/danm_test.go
@@ -7,23 +7,18 @@ import (
 var devicePool0 = "pool0"
 var devicePool1 = "pool1"
 
-var allocatedDevices = make(map[string]*[]string)
+func TestPopDevice(t *testing.T) {
+  allocatedDevices := make(map[string]*[]string)
 
-func TestPopDevicePanic(t *testing.T) {
-    defer func() {
-        if r := recover(); r == nil {
-            t.Errorf("Uninitialized map should expect error.")
-        }
-    }()
+  device,err := popDevice(devicePool0, allocatedDevices)
+  if err == nil {
+    t.Errorf("Uninitialized map should expect error.")
+  }
 
-    popDevice(devicePool0, allocatedDevices)
-}
-
-func TestPopDevice(t *testing.T) {  
   allocatedDevices[devicePool0] = &[]string{"device0"}
   allocatedDevices[devicePool1] = &[]string{"device1", "device2"}
-  
-  device,err := popDevice(devicePool1, allocatedDevices)
+
+  device,err = popDevice(devicePool1, allocatedDevices)
   if device != "device2"  || err != nil {
     t.Errorf("Received device or error does not match with expectation.")
   }
@@ -32,7 +27,7 @@ func TestPopDevice(t *testing.T) {
     t.Errorf("Received device or error does not match with expectation.")
   }
   device,err = popDevice(devicePool1, allocatedDevices)
-  if device == ""  && err == nil {
+  if err == nil {
     t.Errorf("Empty pool should expect error.")
   }
 }

d3471b34dd0e1f77cfe7a6717206ed56abe63926
Levente Kale <levente.kale@nokia.com>
2019-03-13 23:53:28 +0100

    Renaming the test constants from test case specific names to indicate their purpose. Constants were re-used in multiple test cases at the end, so their names wounded up quite annoying.

--- test/uts/syncher_test/syncher_test.go
+++ test/uts/syncher_test/syncher_test.go
@@ -17,22 +17,22 @@ type result struct {
   cniRes *current.Result
 }
 
-var pushResultInterfaces = []*current.Interface {
+var resultInterfaces = []*current.Interface {
       &current.Interface{Name: "eth0",},
       &current.Interface{Name: "eth1",},
       &current.Interface{Name: "fastpath",},
     }
 
-var pushResultTestConsts = []result {
-  {"sriov", nil, &current.Result{CNIVersion: "0.3.1", Interfaces: pushResultInterfaces },},
+var failingTestConsts = []result {
+  {"sriov", nil, &current.Result{CNIVersion: "0.3.1", Interfaces: resultInterfaces },},
   {"flannel", errors.New("this did not go well"), nil},
-  {"ipvlan", errors.New("neither did this"), &current.Result{CNIVersion: "0.3.1", Interfaces: pushResultInterfaces },},
+  {"ipvlan", errors.New("neither did this"), &current.Result{CNIVersion: "0.3.1", Interfaces: resultInterfaces },},
 }
 
-var aggregatedResultTestConsts = []result {
-  {"sriov", nil, &current.Result{CNIVersion: "0.3.1", Interfaces: pushResultInterfaces },},
+var totalSuccessTestConsts = []result {
+  {"sriov", nil, &current.Result{CNIVersion: "0.3.1", Interfaces: resultInterfaces },},
   {"flannel", nil, nil},
-  {"ipvlan", nil, &current.Result{CNIVersion: "0.4.0", Interfaces: pushResultInterfaces },},
+  {"ipvlan", nil, &current.Result{CNIVersion: "0.4.0", Interfaces: resultInterfaces },},
 }
 
 func setupTest(expectedNumber int, results []result) *syncher.Syncher {
@@ -45,14 +45,14 @@ func setupTest(expectedNumber int, results []result) *syncher.Syncher {
 }
 
 func TestPushResult(t *testing.T) {
-  syncher := setupTest(len(pushResultTestConsts), pushResultTestConsts)
-  if syncher.ExpectedNumOfResults != len(pushResultTestConsts) {
-    t.Errorf("Expected number of stored results in object:%d does not match with the initalized value:%d", syncher.ExpectedNumOfResults, len(pushResultTestConsts))
+  syncher := setupTest(len(failingTestConsts), failingTestConsts)
+  if syncher.ExpectedNumOfResults != len(failingTestConsts) {
+    t.Errorf("Expected number of stored results in object:%d does not match with the initalized value:%d", syncher.ExpectedNumOfResults, len(failingTestConsts))
   }
-  if len(syncher.CniResults) != len(pushResultTestConsts) {
-    t.Errorf("Number of stored results in object:%d does not match with the number we have pushed:%d", len(syncher.CniResults), len(pushResultTestConsts))
+  if len(syncher.CniResults) != len(failingTestConsts) {
+    t.Errorf("Number of stored results in object:%d does not match with the number we have pushed:%d", len(syncher.CniResults), len(failingTestConsts))
   }
-  for index, result := range pushResultTestConsts {
+  for index, result := range failingTestConsts {
     t.Run(result.cniName, func(t *testing.T) {
       if syncher.CniResults[index].CniName != result.cniName {
         t.Errorf("CNI name attribute stored inside object:%s does not match with expected:%s", syncher.CniResults[index].CniName, result.cniName)
@@ -68,7 +68,7 @@ func TestPushResult(t *testing.T) {
 }
 
 func TestGetAggregatedResultSuccess(t *testing.T) {
-  syncher := setupTest(len(aggregatedResultTestConsts)+1, aggregatedResultTestConsts)
+  syncher := setupTest(len(totalSuccessTestConsts)+1, totalSuccessTestConsts)
   go addResultToSyncher(syncher,result{"ipvlan", nil, nil})
   err := syncher.GetAggregatedResult()
   if err != nil {
@@ -77,7 +77,7 @@ func TestGetAggregatedResultSuccess(t *testing.T) {
 }
 
 func TestGetAggregatedResultFail(t *testing.T) {
-  syncher := setupTest(len(aggregatedResultTestConsts)+1, aggregatedResultTestConsts)
+  syncher := setupTest(len(totalSuccessTestConsts)+1, totalSuccessTestConsts)
   startTime := time.Now()
   go addResultToSyncher(syncher,result{"ipvlan", errors.New("not this time"), nil})
   err := syncher.GetAggregatedResult()
@@ -93,7 +93,7 @@ func TestGetAggregatedResultFail(t *testing.T) {
 }
 
 func TestGetAggregatedResultTimeout(t *testing.T) {
-  syncher := setupTest(len(aggregatedResultTestConsts)+1, aggregatedResultTestConsts)
+  syncher := setupTest(len(totalSuccessTestConsts)+1, totalSuccessTestConsts)
   startTime := time.Now()
   err := syncher.GetAggregatedResult()
   endTime := time.Now()
@@ -108,10 +108,10 @@ func TestGetAggregatedResultTimeout(t *testing.T) {
 }
 
 func TestMergeCniResults(t *testing.T) {
-  syncher := setupTest(len(pushResultTestConsts), pushResultTestConsts)
+  syncher := setupTest(len(failingTestConsts), failingTestConsts)
   cniResult := syncher.MergeCniResults()
   var expectedNumberOfCniInterfaces int
-  for _, result := range pushResultTestConsts {
+  for _, result := range failingTestConsts {
     if result.cniRes != nil {
       expectedNumberOfCniInterfaces = expectedNumberOfCniInterfaces + len(result.cniRes.Interfaces)
     }
@@ -126,11 +126,11 @@ func TestWasAnyOperationErroneous(t *testing.T) {
   if emptySyncher.WasAnyOperationErroneous() {
      t.Errorf("An empty Syncher object shall not think that any CNI operations were erroneous!")  
   }
-  failSyncher := setupTest(len(pushResultTestConsts), pushResultTestConsts)
+  failSyncher := setupTest(len(failingTestConsts), failingTestConsts)
   if !failSyncher.WasAnyOperationErroneous() {
      t.Errorf("A Syncher object with failing operations shall not think that all CNI operations were successful!")  
   }
-  successSyncher := setupTest(len(aggregatedResultTestConsts), aggregatedResultTestConsts)
+  successSyncher := setupTest(len(totalSuccessTestConsts), totalSuccessTestConsts)
   if successSyncher.WasAnyOperationErroneous() {
      t.Errorf("A Syncher object with only successful operations shall not think that any CNI operations failed!")  
   }

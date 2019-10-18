cf8e93acef57f56451a33762e43ed613fa27f49b
Levente Kale <levente.kale@nokia.com>
2019-10-18 14:16:27 +0200

    Introducing support to interrogate version informaton from project binaries

--- cmd/danm/danm.go
+++ cmd/danm/danm.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "flag"
   "log"
   "os"
   "github.com/containernetworking/cni/pkg/skel"
@@ -8,7 +9,18 @@ import (
   "github.com/nokia/danm/pkg/metacni"
 )
 
+var(
+  version, commitHash string
+)
+
 func main() {
+  printVersion := flag.Bool("version", false, "prints Git version information of the binary to standard out")
+  flag.Parse()
+  if *printVersion {
+    log.Println("DANM binary was built from release: " + version)
+    log.Println("DANM binary was built from commit: " + commitHash)
+    return
+  }
   var err error
   f, err := os.OpenFile("/var/log/danm.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0640)
   if err != nil {
--- scm/build/build.sh
+++ scm/build/build.sh
@@ -14,10 +14,12 @@ go install k8s.io/code-generator/cmd/informer-gen
 deepcopy-gen --alsologtostderr --input-dirs github.com/nokia/danm/crd/apis/danm/v1 -O zz_generated.deepcopy --bounding-dirs github.com/nokia/danm/crd/apis
 client-gen --alsologtostderr --clientset-name versioned --input-base "" --input github.com/nokia/danm/crd/apis/danm/v1 --clientset-path github.com/nokia/danm/crd/client/clientset
 lister-gen --alsologtostderr --input-dirs github.com/nokia/danm/crd/apis/danm/v1 --output-package github.com/nokia/danm/crd/client/listers
-informer-gen --alsologtostderr --input-dirs github.com/nokia/danm/crd/apis/danm/v1 --versioned-clientset-package github.com/nokia/danm/crd/client/clientset/versioned --listers-package github.com/nokia/danm/crd/client/listers --output-package github.com/nokia/danm/crd/client/informers 
-go install -a -ldflags '-extldflags "-static"' github.com/nokia/danm/cmd/danm
-go install -a -ldflags '-extldflags "-static"' github.com/nokia/danm/cmd/netwatcher
-go install -a -ldflags '-extldflags "-static"' github.com/nokia/danm/cmd/fakeipam
-go install -a -ldflags '-extldflags "-static"' github.com/nokia/danm/cmd/svcwatcher
-go install -a -ldflags '-extldflags "-static"' github.com/nokia/danm/cmd/cnitest
-go install -a -ldflags '-extldflags "-static"' github.com/nokia/danm/cmd/webhook
+informer-gen --alsologtostderr --input-dirs github.com/nokia/danm/crd/apis/danm/v1 --versioned-clientset-package github.com/nokia/danm/crd/client/clientset/versioned --listers-package github.com/nokia/danm/crd/client/listers --output-package github.com/nokia/danm/crd/client/informers
+LATEST_TAG=$(git describe --tags)
+COMMIT_HASH=$(git rev-parse HEAD)
+go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/danm
+go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/netwatcher
+go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/svcwatcher
+go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/webhook
+go install -a -ldflags "-extldflags '-static'" github.com/nokia/danm/cmd/fakeipam
+go install -a -ldflags "-extldflags '-static'" github.com/nokia/danm/cmd/cnitest

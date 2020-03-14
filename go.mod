0959d8f32ac8579b22aa86048968eb7eda1b2fe4
MATE Gabor <gabor.mate@nokia.com>
2020-03-14 14:25:10 +0000

    Migrate to go modules, update kubernetes version
    Change build.sh to speed up build and remove unfixed dependencies
    Add hack/tools.go enables k8s.io/code-generator recognised as go.mod dependency
    Update k8s.io/kubernetes to v1.14.10
    Update k8s.io/{api,apimachinery,client-go,code-generator} to v1.17.3
    Remove glide
    Cleanup gitignore

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/nokia/danm
+
+go 1.12
+
+require (
+	github.com/apparentlymart/go-cidr v1.0.1
+	github.com/containernetworking/cni v0.7.1
+	github.com/containernetworking/plugins v0.8.5
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/intel/multus-cni v0.0.0-20191029173424-8c76fd62e0e9
+	github.com/intel/sriov-cni v2.1.0+incompatible
+	github.com/j-keck/arping v1.0.0
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+	github.com/vishvananda/netlink v1.1.1-0.20200221165523-c79a4b7b4066
+	k8s.io/api v0.17.3
+	k8s.io/apimachinery v0.17.3
+	k8s.io/client-go v0.17.3
+	k8s.io/code-generator v0.17.3
+	k8s.io/kubernetes v1.14.10
+)
--- scm/build/build.sh
+++ scm/build/build.sh
@@ -1,15 +1,13 @@
 #!/bin/sh -ex
 export GOOS=linux
-cd $GOPATH/src/github.com/nokia/danm
-glide install --strip-vendor
-go get -d github.com/vishvananda/netlink
-go get github.com/containernetworking/plugins/pkg/ns
-go get github.com/golang/groupcache/lru
+# Force turn on go modules feature becase the project is inside GOPATH
+export GO111MODULE=on
+# Force turn off CGO enables building pure static binaries, otherwise
+# built binary still depends on and dinamically linked against the build
+# environments standard library implementation (e.g. glibc/musl/...)
+export CGO_ENABLED=0
+cd "${GOPATH}/src/github.com/nokia/danm"
+go mod vendor
 LATEST_TAG=$(git describe --tags)
 COMMIT_HASH=$(git rev-parse HEAD)
-go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/danm
-go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/netwatcher
-go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/svcwatcher
-go install -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/webhook
-go install -a -ldflags "-extldflags '-static'" github.com/nokia/danm/cmd/fakeipam
-go install -a -ldflags "-extldflags '-static'" github.com/nokia/danm/cmd/cnitest
+go install -mod=vendor -a -ldflags "-extldflags '-static' -X main.version=${LATEST_TAG} -X main.commitHash=${COMMIT_HASH}" github.com/nokia/danm/cmd/...
